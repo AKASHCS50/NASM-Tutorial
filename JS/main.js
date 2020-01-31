0387396cb2a991792d453207300941cbb14eb3b4
AKASHCS50 <akcount121@gmail.com>
2020-02-01 00:13:58 +0530

    Added Codeblock

--- JS/main.js
+++ JS/main.js
@@ -11,23 +11,3 @@ if($(window).width() >= 760)
       $("#sidebarCollapse").click();      
 }});
 
-$(function() {
-
-    var $sidebar   = $("#sidebar"), 
-        $window    = $(window),
-        offset     = $sidebar.offset(),
-        topPadding = 90;
-
-    $window.scroll(function() {
-        if ($window.scrollTop() > offset.top) {
-            $sidebar.stop().animate({
-                marginTop: $window.scrollTop() - offset.top + topPadding
-            });
-        } else {
-            $sidebar.stop().animate({
-                marginTop: 0
-            });
-        }
-    });
-    
-});

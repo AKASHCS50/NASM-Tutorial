5f756ac06e34ef63901821d7c851b13ca03c5371
AKASHCS50 <akcount121@gmail.com>
2020-01-31 20:26:12 +0530

    Added base html file

--- JS/main.js
+++ JS/main.js
@@ -1,19 +1,33 @@
-(function($) {
+$(document).ready(function() {
+    $("#nav-icon").click(function() {
+        $(this).toggleClass('open');
+    });
+});
 
-	"use strict";
 
-	var fullHeight = function() {
+$(document).ready(function() {
+if($(window).width() >= 760)
+{
+      $("#sidebarCollapse").click();      
+}});
 
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
-			$('.js-fullheight').css('height', $(window).height());
-		});
+$(function() {
 
-	};
-	fullHeight();
+    var $sidebar   = $("#sidebar"), 
+        $window    = $(window),
+        offset     = $sidebar.offset(),
+        topPadding = 90;
 
-	$('#sidebarCollapse').on('click', function () {
-      $('#sidebar').toggleClass('active');
-  });
-
-})(jQuery);
+    $window.scroll(function() {
+        if ($window.scrollTop() > offset.top) {
+            $sidebar.stop().animate({
+                marginTop: $window.scrollTop() - offset.top + topPadding
+            });
+        } else {
+            $sidebar.stop().animate({
+                marginTop: 0
+            });
+        }
+    });
+    
+});
